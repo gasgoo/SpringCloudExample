f3fdc1b5df7c7aef7d4a31a37f0a6fa3d94dc208
raogugen <raogugen@magfin.cn>
2020-04-20 18:29:42 +0800

    add producer for wait/nofity

---
+++ demo-common/src/main/java/com/thread/ExpressTest.java
@@ -0,0 +1,37 @@
+package com.thread;
+
+/**
+ * @Date 2020/4/20 17:54
+ * @name ExpressTest
+ */
+
+
+public class ExpressTest {
+    private static Express express=new Express(0,Express.city);
+
+
+    private static class CheckKm extends Thread{
+        @Override
+        public void run(){
+            express.waitKm();
+        }
+    }
+    private static class CheckSite extends Thread{
+        @Override
+        public void run(){
+            express.waitSite();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for(int i=0;i<3;i++){
+            new CheckKm().start();
+        }
+        for(int i=0;i<3;i++){
+            new CheckSite().start();
+        }
+        Thread.sleep(1000);
+        express.checkKm();
+        express.checkSite();
+    }
+}
