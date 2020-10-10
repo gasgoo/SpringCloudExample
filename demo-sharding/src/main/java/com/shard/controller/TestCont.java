822b981c628991b725ca1cd89990d03daac65e5e
gasgoo <568348991@qq.com>
2020-10-10 18:29:23 +0800

    分库分表改造成2库3表统一sql

---
+++ demo-sharding/src/main/java/com/shard/controller/TestCont.java
@@ -0,0 +1,11 @@
+package com.shard.controller;
+
+/**
+ * @Desc
+ * @Date 2020/10/10 10:43
+ **/
+public class TestCont {
+
+    public static void main(String[] args) {
+    }
+}
--- demo-sharding/src/main/resources/sql/table.sql
+++ demo-sharding/src/main/resources/sql/table.sql
@@ -1,28 +1,6 @@
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for t_student_00
--- ----------------------------
-DROP TABLE IF EXISTS `t_student_00`;
-CREATE TABLE `t_student_00` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for t_student_01
--- ----------------------------
-DROP TABLE IF EXISTS `t_student_01`;
-CREATE TABLE `t_student_01` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for t_user_00
